221e40cc87e3edcd992c7d0934b7e2e82993624e
Yazan-Almutte-CE <yazan.abd.almutte@gmail.com>
2025-04-19 22:11:05 +0300

    Updated

--- bin/dsa_in_dart.dart
+++ bin/dsa_in_dart.dart
@@ -1,22 +1,36 @@
 import 'package:dsa_in_dart/algorthims/huffman_code.dart';
+import 'package:dsa_in_dart/stack.dart';
 
 void main() {
 
 
 
+  //Stack Main Code
+  /*
+  Stack<String> stack = Stack<String>();
+  stack.push("Yazan");
+  print('--Push--');
+  stack.push("Shrouf");
+  print('--Push--');
+  stack.push("Ahmad");
+  print('--Push--');
+  print('Last Element is: ${stack.peek()}');
+  print('size is: ${stack.size()}');
+  print('--Pop--');
+  stack.pop();
+  print('Last Element is: ${stack.peek()}');
+  print('size is: ${stack.size()}');
+  stack.clear();
+  print('--Clear--');
+  print('size is: ${stack.size()}');
+  print('is Stack Empty?: ${stack.isEmpty()}');
+  print('Last Element is: ${stack.peek()}');
 
+*/
 
-
-
-
-
-
-
-
-  //Huffman  Main Code
+  //Huffman Main Code
   /*  
   Map<String, int> frequencies = {'A': 40, 'B': 35, 'C': 20, 'D': 5};
-
   HuffmanCoding huffmanCoding = HuffmanCoding();
   huffmanCoding.buildHuffmanTree(frequencies);
   print('Huffman Codes:');
